c31185d4bf92f416e5e6151d6a1167dc45e8c11c
mhmharshani <mhmharshani@gmail.com>
2025-03-09 16:57:14 +0530

    Model(dto) Classes

--- src/main/java/edu/icet/dto/User.java
+++ src/main/java/edu/icet/dto/User.java
@@ -1,5 +1,26 @@
 package edu.icet.dto;
 
+import edu.icet.util.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    private Integer id;
+    private String userName;
+    private String password;
+    private String email;
+    private UserRole role;
+    private String address;
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
